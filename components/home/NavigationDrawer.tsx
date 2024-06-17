cf12a01a4e9416dd825e82776d0c3dca98d6835a
tianbao <mtian999@163.com>
2024-06-17 22:44:50 +0800

    feat: secend commit

--- components/home/NavigationDrawer.tsx
+++ components/home/NavigationDrawer.tsx
@@ -3,28 +3,28 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import { useEffect, useState } from 'react';
-import { usePathname, useRouter } from 'next/navigation';
+// import { usePathname, useRouter } from 'next/navigation';
 import { useTranslations } from 'next-intl';
 
-import { NAV_LINKS } from '@/lib/constants';
+// import { NAV_LINKS } from '@/lib/constants';
 import { cn } from '@/lib/utils';
 
-function NavDrawerItem({ isActive, name }: { isActive: boolean; name: string }) {
-  return (
-    <li
-      className={cn(
-        'flex h-[28px] w-full items-center justify-between rounded-[4px] border border-transparent bg-[#2C2D36] pl-[14px] pr-2',
-        isActive && 'border-white bg-[#15141A]',
-      )}
-    >
-      <div className={cn('size-3 rounded-full bg-[#15141A]', isActive && 'bg-white')} />
-      <div className={cn('text-sm text-white/40', isActive && 'text-white')}>{name}</div>
-    </li>
-  );
-}
+// function NavDrawerItem({ isActive, name }: { isActive: boolean; name: string }) {
+//   return (
+//     <li
+//       className={cn(
+//         'flex h-[28px] w-full items-center justify-between rounded-[4px] border border-transparent bg-[#2C2D36] pl-[14px] pr-2',
+//         isActive && 'border-white bg-[#15141A]',
+//       )}
+//     >
+//       <div className={cn('size-3 rounded-full bg-[#15141A]', isActive && 'bg-white')} />
+//       <div className={cn('text-sm text-white/40', isActive && 'text-white')}>{name}</div>
+//     </li>
+//   );
+// }
 
 export default function NavigationDrawer({ open, setOpen }: { open: boolean; setOpen: (open: boolean) => void }) {
-  const t = useTranslations('Navigation');
+  // const t = useTranslations('Navigation');
   // const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(open);
   // const router = useRouter();
