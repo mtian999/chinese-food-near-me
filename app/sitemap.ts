83ddd77e29d53f24f81704a0e794ad1d5e32e271
tianbao <mtian999@163.com>
2024-06-18 17:30:11 +0800

    fix:修改sitemap,优化seo

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,7 @@
 import { type MetadataRoute } from 'next';
 import { locales } from '@/i18n';
 
+import { dataList } from '@/lib/data';
 import { BASE_URL } from '@/lib/env';
 
 export default function sitemap(): MetadataRoute.Sitemap {
@@ -17,20 +18,29 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'daily',
       priority: 1,
     },
-    {
-      url: 'submit',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'startup',
+    // {
+    //   url: 'submit',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'monthly',
+    //   priority: 0.8,
+    // },
+    // {
+    //   url: 'startup',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'daily',
+    //   priority: 0.8,
+    // },
+  ];
+  dataList.forEach((dataListItem) => {
+    let routeUrl = dataListItem.name;
+    routeUrl = routeUrl === '' ? '' : `delicious/${routeUrl}`;
+    sitemapRoutes.push({
+      url: routeUrl,
       lastModified: new Date(),
       changeFrequency: 'daily',
-      priority: 0.8,
-    },
-  ];
-
+      priority: 0.9,
+    });
+  });
   const sitemapData = sitemapRoutes.flatMap((route) =>
     locales.map((locale) => {
       const lang = locale === 'en' ? '' : `/${locale}`;
